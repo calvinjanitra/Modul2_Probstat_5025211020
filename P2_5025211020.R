136a6805047487d3ec33641e8f7666723e98683f
calvinjanitra <95208578+calvinjanitra@users.noreply.github.com>
2022-12-10 15:02:34 +0700

    Add files via upload

---
+++ P2_5025211020.R
@@ -0,0 +1,38 @@
+# 1 A
+before <- c(78, 75, 67, 77, 70, 72, 78, 74, 77)
+after <- c(100, 95, 70, 90, 90, 90, 89, 90, 100)
+difference <- before - after
+sd(difference)
+
+# 1 B
+t.test(after, before, paired = TRUE)
+
+
+# 3 A
+H0: miu1 = miu2 
+H1: miu1 != miu2 
+
+# 3 B
+install.packages("BSDA")
+library(BSDA)
+
+tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, 
+          mean.y =2.79 , s.y = 1.32, n.y = 27, 
+          alternative = "greater", var.equal = TRUE)
+
+# 4 A
+myFile  <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"))
+dim(myFile)
+head(myFile)
+attach(myFile)
+
+myFile$V1 <- as.factor(myFile$V1)
+myFile$V1 = factor(myFile$V1,labels = c("Kucing Oren","Kucing Hitam","Kucing Putih","Kucing Oren"))
+
+class(myFile$V1)
+
+group1 <- subset(myFile, V1=="Kucing Oren")
+group2 <- subset(myFile, V1=="Kucing Hitam")
+group3 <- subset(myFile, V1=="Kucing Putih")
+
+# 4 B
